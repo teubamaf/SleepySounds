8ba0557defbbea8dc4808b553d689d0723e189ae
Amaury <amaury.durand-noel@hotmail.fr>
2024-03-04 14:37:38 +0100

    Initial Commit

---
+++ SleepySounds/SleepySoundsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SleepySoundsApp.swift
+//  SleepySounds
+//
+//  Created by Amaury Durand-Noel on 04/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct SleepySoundsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
