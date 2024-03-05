7e536725826485236d28afe83e40f4e397d9fbe0
Amaury <amaury.durand-noel@hotmail.fr>
2024-03-05 13:23:44 +0100

    Adding new page

---
+++ SleepySounds/ Views/SoundDetail.swift
@@ -0,0 +1,18 @@
+//
+//  SoundDetail.swift
+//  SleepySounds
+//
+//  Created by Amaury Durand-Noel on 05/03/2024.
+//
+
+import SwiftUI
+
+struct SoundDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SoundDetail()
+}
